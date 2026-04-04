ae4c5c6e459746abd283087b7221218480408d89
carlotata <avisojohn040@gmail.com>
2026-04-04 19:01:21 +0800

    feat: added openrouter, widget chat assistant

--- app/(guest)/layout.tsx
+++ app/(guest)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/common/header-nav";
 import Footer from "@/components/common/footer";
 import { Geist, Geist_Mono } from "next/font/google";
+import ChatWidget from "@/components/ui/aichatwidget";
 import "@/styles/globals.css";
 <link
    href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;1,400&display=swap"
@@ -23,12 +24,12 @@ export default function GuestLayout({
    children: React.ReactNode;
 }>) {
    return (
-         <div
-            className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-[#dedcdc] dark:bg-[#181818] transition-colors duration-300`}>
-            <Navbar />
-            {children}
-            <Footer />
-         </div>
-
+      <div
+         className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-[#dedcdc] dark:bg-[#181818] transition-colors duration-300`}>
+         <Navbar />
+         {children}
+         <ChatWidget />
+         <Footer />
+      </div>
    );
 }
